e8ca3412fe58fe416edffe9dc7f0438f8a4c26c9
Erik De Rijcke <derijcke.erik@gmail.com>
2020-10-02 22:26:00 +0200

    xwayland (wip)

--- compositor-module/src/xwayland/XWindowManager.ts
+++ compositor-module/src/xwayland/XWindowManager.ts
@@ -1,4 +1,5 @@
 import {
+  ATOM,
   Atom,
   ButtonPressEvent,
   ButtonReleaseEvent,
@@ -14,6 +15,7 @@ import {
   EventMask,
   FocusInEvent,
   getComposite,
+  GetPropertyReply,
   getRender,
   getXFixes,
   LeaveNotifyEvent,
@@ -35,10 +37,71 @@ import {
 } from 'xtsb'
 import { XWaylandConnection } from './XWaylandConnection'
 
+type ConfigureValueList = Parameters<XConnection['configureWindow']>
+type MwmDecor = number
+
+const MWM_DECOR_ALL: 1 = 1
+const MWM_DECOR_BORDER: 2 = 2
+const MWM_DECOR_RESIZEH: 4 = 4
+const MWM_DECOR_TITLE: 8 = 8
+const MWM_DECOR_MENU: 16 = 16
+const MWM_DECOR_MINIMIZE: 32 = 32
+const MWM_DECOR_MAXIMIZE: 64 = 64
+
+const MWM_DECOR_EVERYTHING: 126 = 126
+
+const MWM_HINTS_FUNCTIONS: 1 = 1
+const MWM_HINTS_DECORATIONS: 2 = 2
+const MWM_HINTS_INPUT_MODE: 4 = 4
+const MWM_HINTS_STATUS: 8 = 8
+
+const MWM_FUNC_ALL: 1 = 1
+const MWM_FUNC_RESIZE: 2 = 2
+const MWM_FUNC_MOVE: 4 = 4
+const MWM_FUNC_MINIMIZE: 8 = 8
+const MWM_FUNC_MAXIMIZE: 16 = 16
+const MWM_FUNC_CLOSE: 32 = 32
+
+const MWM_INPUT_MODELESS: 0 = 0
+const MWM_INPUT_PRIMARY_APPLICATION_MODAL: 1 = 1
+const MWM_INPUT_SYSTEM_MODAL: 2 = 2
+const MWM_INPUT_FULL_APPLICATION_MODAL: 3 = 3
+const MWM_INPUT_APPLICATION_MODAL = MWM_INPUT_PRIMARY_APPLICATION_MODAL
+
+const MWM_TEAROFF_WINDOW: 1 = 1
+
+const _NET_WM_MOVERESIZE_SIZE_TOPLEFT: 0 = 0
+const _NET_WM_MOVERESIZE_SIZE_TOP: 1 = 1
+const _NET_WM_MOVERESIZE_SIZE_TOPRIGHT: 2 = 2
+const _NET_WM_MOVERESIZE_SIZE_RIGHT: 3 = 3
+const _NET_WM_MOVERESIZE_SIZE_BOTTOMRIGHT: 4 = 4
+const _NET_WM_MOVERESIZE_SIZE_BOTTOM: 5 = 5
+const _NET_WM_MOVERESIZE_SIZE_BOTTOMLEFT: 6 = 6
+const _NET_WM_MOVERESIZE_SIZE_LEFT: 7 = 7
+const _NET_WM_MOVERESIZE_MOVE: 8 = 8   /* movement only */
+const _NET_WM_MOVERESIZE_SIZE_KEYBOARD: 9 = 9   /* size via keyboard */
+const _NET_WM_MOVERESIZE_MOVE_KEYBOARD: 10 = 10   /* move via keyboard */
+const _NET_WM_MOVERESIZE_CANCEL: 11 = 11   /* cancel operation */
+
+const ICCCM_WITHDRAWN_STATE: 0 = 0
+const ICCCM_NORMAL_STATE: 1 = 1
+const ICCCM_ICONIC_STATE: 3 = 3
+
 type NamedAtom = [
   name: string,
   value: number
 ]
+
+/* We reuse some predefined, but otherwise useles atoms
+ * as local type placeholders that never touch the X11 server,
+ * to make weston_wm_window_read_properties() less exceptional.
+ */
+const TYPE_WM_PROTOCOLS = Atom.cutBuffer0
+const TYPE_MOTIF_WM_HINTS = Atom.cutBuffer1
+const TYPE_NET_WM_STATE = Atom.cutBuffer2
+const TYPE_WM_NORMAL_HINTS = Atom.cutBuffer3
+
+type Prop = [atom: ATOM, type: ATOM, propUpdater: (prop: GetPropertyReply) => void]
 
 type XWMAtoms = {
   WM_PROTOCOLS: number
@@ -120,6 +183,64 @@ interface XWindowManagerResources {
 interface VisualAndColormap {
   visualId: number,
   colormap: number
+}
+
+interface SizeHints {
+  flags: number,
+  x: number
+  y: number
+  width: number,
+  height: number
+  minWidth: number,
+  minHeight: number,
+  maxWidth: number,
+  maxHeight: number,
+  widthInc: number
+  heightInc: number
+  minAspect: { x: number, y: number }
+  maxAspect: { x: number, y: number }
+  baseWidth: number,
+  baseHeight: number,
+  winGravity: number
+}
+
+interface MotifWmHints {
+  flags: number,
+  functions: number,
+  decorations: number,
+  inputMode: number,
+  status: number
+}
+
+class DesktopXwaylandSurface {
+  // TODO handles
+}
+
+interface WmWindow {
+  width: number
+  height: number
+  frameId: WINDOW
+  x: number
+  y: number
+  mapRequestX: number
+  mapRequestY: number
+  maximizedHorizontal: boolean
+  maximizedVertical: boolean
+  fullscreen: boolean
+  id: WINDOW
+  class: string
+  name: string
+  transientFor: WmWindow
+  type: ATOM
+  pid: number
+  machine: string
+  propertiesDirty: boolean
+  overrideRedirect: boolean
+  decorate: MwmDecor
+  sizeHints: SizeHints
+  motifHints: MotifWmHints
+  deleteWindow: boolean
+  shsurf: DesktopXwaylandSurface
 }
 
 async function setupResources(xConnection: XConnection): Promise<XWindowManagerResources> {
@@ -277,53 +398,51 @@ function setNetActiveWindow(xConnection: XConnection, screen: SCREEN, xwmAtoms: 
   xConnection.changeProperty(PropMode.Replace, screen.root, xwmAtoms._NET_ACTIVE_WINDOW, xwmAtoms.WINDOW, 32, new Uint32Array([window]))
 }
 
-async function createWMWindow(xConnection: XConnection, screen: SCREEN, xwmAtoms: XWMAtoms) {
+function createWMWindow(xConnection: XConnection, screen: SCREEN, xwmAtoms: XWMAtoms) {
   const wmWindow = xConnection.allocateID()
-  await Promise.all([
-    xConnection.createWindow(
-      WindowClass.CopyFromParent,
-      wmWindow,
-      screen.root,
-      0,
-      0,
-      10,
-      10,
-      0,
-      WindowClass.InputOutput,
-      screen.rootVisual,
-      {}
-    ).check(),
+  xConnection.createWindow(
+    WindowClass.CopyFromParent,
+    wmWindow,
+    screen.root,
+    0,
+    0,
+    10,
+    10,
+    0,
+    WindowClass.InputOutput,
+    screen.rootVisual,
+    {}
+  )
 
-    xConnection.changeProperty(
-      PropMode.Replace,
-      wmWindow,
-      xwmAtoms._NET_SUPPORTING_WM_CHECK,
-      Atom.WINDOW,
-      32,
-      new Uint32Array([wmWindow])
-    ).check(),
+  xConnection.changeProperty(
+    PropMode.Replace,
+    wmWindow,
+    xwmAtoms._NET_SUPPORTING_WM_CHECK,
+    Atom.WINDOW,
+    32,
+    new Uint32Array([wmWindow])
+  )
 
-    xConnection.changeProperty(
-      PropMode.Replace,
-      wmWindow,
-      xwmAtoms._NET_WM_NAME,
-      xwmAtoms.UTF8_STRING,
-      8,
-      chars('Greenfield WM')
-    ).check(),
+  xConnection.changeProperty(
+    PropMode.Replace,
+    wmWindow,
+    xwmAtoms._NET_WM_NAME,
+    xwmAtoms.UTF8_STRING,
+    8,
+    chars('Greenfield WM')
+  )
 
-    xConnection.changeProperty(
-      PropMode.Replace,
-      screen.root,
-      xwmAtoms._NET_SUPPORTING_WM_CHECK,
-      Atom.WINDOW,
-      32,
-      new Uint32Array([wmWindow])
-    ).check(),
+  xConnection.changeProperty(
+    PropMode.Replace,
+    screen.root,
+    xwmAtoms._NET_SUPPORTING_WM_CHECK,
+    Atom.WINDOW,
+    32,
+    new Uint32Array([wmWindow])
+  )
 
-    xConnection.setSelectionOwner(wmWindow, xwmAtoms.WM_S0, Time.CurrentTime).check(),
-    xConnection.setSelectionOwner(wmWindow, xwmAtoms._NET_WM_CM_S0, Time.CurrentTime).check()
-  ])
+  xConnection.setSelectionOwner(wmWindow, xwmAtoms.WM_S0, Time.CurrentTime)
+  xConnection.setSelectionOwner(wmWindow, xwmAtoms._NET_WM_CM_S0, Time.CurrentTime)
 
   return wmWindow
 }
@@ -366,7 +485,7 @@ export class XWindowManager {
     // TODO
     createCursor()
 
-    const wmWindow = await createWMWindow(xConnection, xConnection.setup.roots[0], xwmAtoms)
+    const wmWindow = createWMWindow(xConnection, xConnection.setup.roots[0], xwmAtoms)
 
 
     const xWindowManager = new XWindowManager(xConnection, xConnection.setup.roots[0], xWmResources, visualAndColormap, wmWindow)
@@ -410,6 +529,7 @@ export class XWindowManager {
   private readonly colormap: number
   private readonly screen: SCREEN
   private readonly wmWindow: WINDOW
+  private readonly windowHash: { [key: number]: WmWindow } = {}
 
   constructor(
     xConnection: XConnection,
@@ -451,8 +571,46 @@ export class XWindowManager {
 // TODO
   }
 
-  private handleMapRequest(event: MapRequestEvent) {
-// TODO
+  private async handleMapRequest(event: MapRequestEvent) {
+    if (this.isOurResource(event.window)) {
+      return
+    }
+
+    const window = this.lookupWindow(event.window)
+    if (window === undefined) {
+      return
+    }
+
+    await this.windowReadProperties(window)
+
+    /* For a new Window, MapRequest happens before the Window is realized
+     * in Xwayland. We do the real xcb_map_window() here as a response to
+     * MapRequest. The Window will get realized (wl_surface created in
+     * Wayland and WL_SURFACE_ID sent in X11) when it has been mapped for
+     * real.
+     *
+     * MapRequest only happens for (X11) unmapped Windows. On UnmapNotify,
+     * we reset shsurf to NULL, so even if X11 connection races far ahead
+     * of the Wayland connection and the X11 client is repeatedly mapping
+     * and unmapping, we will never have shsurf set on MapRequest.
+     */
+    if (window.shsurf !== undefined) {
+      throw new Error('Assertion failed. X window should not have a shell surface on map request.')
+    }
+
+    window.mapRequestX = window.x
+    window.mapRequestY = window.y
+
+    if (window.frameId === Window.None) {
+      this.createFrame(window) /* sets frame_id */
+    }
+    if (window.frameId === Window.None) {
+      throw new Error('Assertion failed. X window should have a parent window.')
+    }
+
+    this.setAllowCommits(window, false)
+    this.setWmState(window, ICCCM_NORMAL_STATE)
+    // TODO more
   }
 
   private handleMapNotify(event: MapNotifyEvent) {
@@ -489,5 +647,188 @@ export class XWindowManager {
 
   private handleFocusIn(event: FocusInEvent) {
 // TODO
+  }
+
+  private isOurResource(id: number) {
+    const { resourceIdMask, resourceIdBase } = this.xConnection.setup
+    return (id & ~resourceIdMask) === resourceIdBase
+  }
+
+  private lookupWindow(window: WINDOW) {
+    return this.windowHash[window]
+  }
+
+  private async windowReadProperties(window: WmWindow) {
+    if (!window.propertiesDirty) {
+      return
+    }
+    window.propertiesDirty = false
+
+    const props: Prop[] = [
+      [Atom.wmClass, Atom.STRING, ({ value }) => window.class = value.chars()],
+      [Atom.wmName, Atom.STRING, ({ value }) => window.name = value.chars()],
+      [Atom.wmTransientFor, Atom.WINDOW, ({ value }) => {
+        const lookupWindow = this.lookupWindow(new Uint32Array(value.buffer)[0])
+        if (lookupWindow === undefined) {
+          console.log('XCB_ATOM_WINDOW contains window id not found in hash table.')
+        } else {
+          window.transientFor = lookupWindow
+        }
+      }],
+      [this.xwmAtoms.WM_PROTOCOLS, TYPE_WM_PROTOCOLS, ({ value, valueLen }) => {
+        const atoms = new Uint32Array(value.buffer)
+        for (let i = 0; i < valueLen; i++) {
+          if (atoms[i] === this.xwmAtoms.WM_DELETE_WINDOW) {
+            window.deleteWindow = true
+            break
+          }
+        }
+      }],
+      [this.xwmAtoms.WM_NORMAL_HINTS, TYPE_WM_NORMAL_HINTS, ({ value }) => {
+        const [flags, x, y, width, height, minWidth, minHeight, maxWidth, maxHeight, widthInc, heightInc, minAspectX, minAspectY, maxAspectX, maxAspectY, baseWidth, baseHeight, winGravity] = new Uint32Array(value.buffer)
+        window.sizeHints = {
+          flags,
+          x,
+          y,
+          width,
+          height,
+          minWidth,
+          minHeight,
+          maxWidth,
+          maxHeight,
+          widthInc,
+          heightInc,
+          minAspect: { x: minAspectX, y: minAspectY },
+          maxAspect: { x: maxAspectX, y: maxAspectY },
+          baseWidth,
+          baseHeight,
+          winGravity
+        }
+      }],
+      [this.xwmAtoms._NET_WM_STATE, TYPE_NET_WM_STATE, ({ value, valueLen }) => {
+        window.fullscreen = false
+        const atoms = new Uint32Array(value.buffer)
+        for (let i = 0; i < valueLen; i++) {
+          if (atoms[i] === this.xwmAtoms._NET_WM_STATE_FULLSCREEN) {
+            window.fullscreen = true
+          }
+          if (atoms[i] === this.xwmAtoms._NET_WM_STATE_MAXIMIZED_VERT) {
+            window.maximizedVertical = true
+          }
+          if (atoms[i] === this.xwmAtoms._NET_WM_STATE_MAXIMIZED_HORZ) {
+            window.maximizedHorizontal = true
+          }
+        }
+      }],
+      [this.xwmAtoms._NET_WM_WINDOW_TYPE, Atom.ATOM, ({ value }) => window.type = new Uint32Array(value.buffer)[0]],
+      [this.xwmAtoms._NET_WM_NAME, Atom.STRING, ({ value }) => window.name = value.chars()],
+      [this.xwmAtoms._NET_WM_PID, Atom.CARDINAL, ({ value }) => window.pid = new Uint32Array(value.buffer)[0]],
+      [this.xwmAtoms._MOTIF_WM_HINTS, TYPE_MOTIF_WM_HINTS, ({ value }) => {
+        const [flags, functions, decorations, inputMode, status] = new Uint32Array(value.buffer)
+        window.motifHints = {
+          flags,
+          functions,
+          decorations,
+          inputMode,
+          status
+        }
+        if (window.motifHints.flags & MWM_HINTS_DECORATIONS) {
+          if (window.motifHints.decorations & MWM_DECOR_ALL) {
+            /* MWM_DECOR_ALL means all except the other values listed. */
+            window.decorate = MWM_DECOR_EVERYTHING & (~window.motifHints.decorations)
+          } else {
+            window.decorate = window.motifHints.decorations
+          }
+        }
+      }],
+      [this.xwmAtoms.WM_CLIENT_MACHINE, Atom.wmClientMachine, ({ value }) => window.machine = value.chars()]
+    ]
+
+    props.forEach(([atom, type, propUpdater]: Prop) =>
+      this.xConnection.getProperty(0, window.id, atom, Atom.Any, 0, 2048)
+        .then(property => {
+          if (property._type === Atom.None) {
+            /* No such property */
+            return
+          }
+          propUpdater(property)
+        })
+        .catch(() => {
+          /* Bad window, typically */
+        }))
+  }
+
+  private createFrame(window: WmWindow) {
+    // TODO paint a nice window bar using canvas2d
+    // TODO see weston window-manager.c weston_wm_window_create_frame for more implementation details
+
+    const topBarHeight = 25
+    const frameWidth = window.width
+    const frameHeight = window.height + topBarHeight
+    const childX = 0
+    const childY = topBarHeight
+
+    window.frameId = this.xConnection.allocateID()
+    this.xConnection.createWindow(
+      32,
+      window.frameId,
+      this.screen.root,
+      0,
+      0,
+      frameWidth,
+      frameHeight,
+      0,
+      WindowClass.InputOutput,
+      this.visualId,
+      {
+        borderPixel: this.screen.blackPixel,
+        eventMask: EventMask.KeyPress |
+          EventMask.KeyRelease |
+          EventMask.ButtonPress |
+          EventMask.ButtonRelease |
+          EventMask.PointerMotion |
+          EventMask.EnterWindow |
+          EventMask.LeaveWindow |
+          EventMask.SubstructureNotify |
+          EventMask.SubstructureRedirect,
+        colormap: this.colormap
+      })
+
+    this.xConnection.reparentWindow(window.id, window.frameId, childX, childY)
+
+    this.configureWindow(window.id, { borderWidth: 0 })
+
+    this.windowHash[window.frameId] = window
+  }
+
+  private configureWindow(id: WINDOW, valueList: ConfigureValueList[1]) {
+    this.xConnection.configureWindow(id, valueList)
+  }
+
+  /** Control Xwayland wl_surface.commit behaviour
+   *
+   * This function sets the "_XWAYLAND_ALLOW_COMMITS" property of the frame window
+   * (not the content window!) to allow.
+   *
+   * If the property is set to true, Xwayland will commit whenever it likes.
+   * If the property is set to false, Xwayland will not commit.
+   * If the property is not set at all, Xwayland assumes it is true.
+   *
+   * @param window The XWM window to control.
+   * @param allow Whether Xwayland is allowed to wl_surface.commit for the window.
+   */
+  private setAllowCommits(window: WmWindow, allow: boolean) {
+    if (window.frameId === undefined) {
+      throw new Error('Window does not have a parent.')
+    }
+
+    this.xConnection.changeProperty(PropMode.Replace, window.frameId, this.xwmAtoms._XWAYLAND_ALLOW_COMMITS, Atom.CARDINAL, 32, new Uint32Array([allow ? 1 : 0]))
+  }
+
+  private setWmState(window: WmWindow, state: number) {
+    this.xConnection.changeProperty(PropMode.Replace, window.id, this.xwmAtoms.WM_STATE, this.xwmAtoms.WM_STATE, 32, new Uint32Array([
+      state,
+      Window.None
+    ]))
   }
 }
